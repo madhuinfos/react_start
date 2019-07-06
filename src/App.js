36f097da74f1a5aed340c56d4ed19dd363753250
madhu s <madhu@madhus-MacBook-Pro.local>
2019-07-06 19:31:05 +0530

    wd wc eeexx
    iiiii
    :wa
    :wq
    	#

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Table from './Table';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+    render() {
+        const business = [
+            {
+                name: 'Amazon',
+                type: 'Online marketing'
+            },
+
+            {
+                name: 'Flipkart',
+                type: 'Indian startup online marketing'
+            },
+
+            {
+                name: 'Ola',
+                type: 'Cabs business'
+            }
+        ]
+
+        this.state = {
+            characters: [
+                {
+                    name: 'From State1',
+                    type: 'state type1'
+                },
+
+                {
+                    name: 'From State2',
+                    type: 'state type2'
+                },
+
+                {
+                    name: 'From State 3',
+                    type: 'state type 3'
+                }
+            ]
+        }
+
+        const {characters} = this.state;
+
+        const removeCharacter = index => {
+            console.log('remove char invoked');
+            this.setState({
+                characters: characters.filter((character, i) =>  {
+                    return i !== index;
+                })
+            });
+        }
+
+        const listItems = characters.map((x) => <li>{x.name}</li>);
+
+        return (
+            
+            <div className = "App">
+                {/* <ul>{business}</ul> */}
+                <Table businessData = {business} removeCharacter = {removeCharacter} />
+                <h1>{typeof(characters)}</h1>
+                <Table businessData = {characters} removeCharacter = {removeCharacter}></Table>
+            </div>
+        )
+    }
 }
 
 export default App;
